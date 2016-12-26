11f3b48ad9d09cc8b2b1c1fbfa8117cebd67b4d6
prabhakar <prabhakar.pandey@games24x7.com>
2016-12-26 20:07:02 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+app = express();
+var http = require('http');
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+
+//app.use('/js', express.static(__dirname + '/build'));
+
+var engines = require('consolidate');
+
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+var webpackMiddleware = require("webpack-dev-middleware");
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+app.use(webpackMiddleware(webpack({
+    entry: APP_DIR + '/index.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.es6']
+    }
+}), {
+    noInfo: false,
+    quiet: false,
+    // lazy: true,
+    publicPath: "/js/",
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: true
+    }
+
+}));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+var server = http.createServer(app).listen(app.get('port'), function() {
+    console.log('I am running on Port' + app.get('port'));
+});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+   loaders: [
+     {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015'] 
+       }
+     }
+   ]
+ },
+ resolve: {
+   extensions: ['', '.js', '.es6']
+ }
+};
+
+
+
+module.exports = config;
